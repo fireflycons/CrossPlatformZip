f036e6efa86f89a51930f189733eff1c611c4fe9
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2019-07-23 05:56:31 +0100

    Default build.ps1

--- build/publish.ps1
+++ build/publish.ps1
@@ -1,5 +1,3 @@
-#!/usr/bin/pwsh
-
 ##########################################################################
 # This is the Cake bootstrapper script for PowerShell.
 # This file was downloaded from https://github.com/cake-build/resources
@@ -23,36 +21,55 @@ The build script target to run.
 The build configuration to use.
 .PARAMETER Verbosity
 Specifies the amount of information to be displayed.
-.PARAMETER Experimental
-Tells Cake to use the latest Roslyn release.
-.PARAMETER WhatIf
-Performs a dry run of the build script.
-No tasks will be executed.
+.PARAMETER ShowDescription
+Shows description about tasks.
+.PARAMETER DryRun
+Performs a dry run.
 .PARAMETER SkipToolPackageRestore
 Skips restoring of packages.
 .PARAMETER ScriptArgs
 Remaining arguments are added here.
 
 .LINK
-http://cakebuild.net
+https://cakebuild.net
 
 #>
 
 [CmdletBinding()]
 Param(
     [string]$Script = "build.cake",
-    [string]$Target = "Default",
-    [ValidateSet("Release", "Debug")]
-    [string]$Configuration = "Release",
+    [string]$Target,
+    [string]$Configuration,
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
-    [string]$Verbosity = "Normal",
-    [switch]$Experimental,
-    [Alias("DryRun", "Noop")]
-    [switch]$WhatIf,
+    [string]$Verbosity,
+    [switch]$ShowDescription,
+    [Alias("WhatIf", "Noop")]
+    [switch]$DryRun,
     [switch]$SkipToolPackageRestore,
     [Parameter(Position = 0, Mandatory = $false, ValueFromRemainingArguments = $true)]
     [string[]]$ScriptArgs
 )
+
+# Attempt to set highest encryption available for SecurityProtocol.
+# PowerShell will not set this by default (until maybe .NET 4.6.x). This
+# will typically produce a message for PowerShell v2 (just an info
+# message though)
+try
+{
+    # Set TLS 1.2 (3072), then TLS 1.1 (768), then TLS 1.0 (192), finally SSL 3.0 (48)
+    # Use integers because the enumeration values for TLS 1.2 and TLS 1.1 won't
+    # exist in .NET 4.0, even though they are addressable if .NET 4.5+ is
+    # installed (.NET 4.5 is an in-place upgrade).
+    # PowerShell Core already has support for TLS 1.2 so we can skip this if running in that.
+    if (-not $IsCoreCLR)
+    {
+        [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48
+    }
+}
+catch
+{
+    Write-Output 'Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed. If you see underlying connection closed or trust errors, you may need to upgrade to .NET Framework 4.5+ and PowerShell v3'
+}
 
 [Reflection.Assembly]::LoadWithPartialName("System.Security") | Out-Null
 function MD5HashFile([string] $filePath)
@@ -72,302 +89,198 @@ function MD5HashFile([string] $filePath)
     }
     finally
     {
-        if ($null -ne $file)
+        if ($file -ne $null)
         {
             $file.Dispose()
         }
     }
 }
 
-try
+function GetProxyEnabledWebClient
 {
-    Write-Host "Preparing to run build script..."
-	Push-Location $PSScriptRoot
+    $wc = New-Object System.Net.WebClient
+    $proxy = [System.Net.WebRequest]::GetSystemWebProxy()
+    $proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+    $wc.Proxy = $proxy
+    return $wc
+}
 
-    Write-Host "Checking operating system..."
-    $IsWindowsOS = (-not (Get-Variable -Name IsWindows -ErrorAction Ignore)) -or $IsWindows
-    $IsLinuxOS = (Get-Variable -Name IsLinux -ErrorAction Ignore) -and $IsLinux
+Write-Host "Preparing to run build script..."
 
-    if ($IsWindowsOS)
+if (!$PSScriptRoot)
+{
+    $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+
+$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
+$ADDINS_DIR = Join-Path $TOOLS_DIR "Addins"
+$MODULES_DIR = Join-Path $TOOLS_DIR "Modules"
+$NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
+$CAKE_EXE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
+$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
+$PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
+$ADDINS_PACKAGES_CONFIG = Join-Path $ADDINS_DIR "packages.config"
+$MODULES_PACKAGES_CONFIG = Join-Path $MODULES_DIR "packages.config"
+
+# Make sure tools folder exists
+if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR))
+{
+    Write-Verbose -Message "Creating tools directory..."
+    New-Item -Path $TOOLS_DIR -Type Directory | Out-Null
+}
+
+# Make sure that packages.config exist.
+if (!(Test-Path $PACKAGES_CONFIG))
+{
+    Write-Verbose -Message "Downloading packages.config..."
+    try
     {
-        Write-Host " - Windows"
+        $wc = GetProxyEnabledWebClient
+        $wc.DownloadFile("https://cakebuild.net/download/bootstrapper/packages", $PACKAGES_CONFIG)
     }
-    elseif ($IsLinuxOS)
+    catch
     {
-        Write-Host " - Linux"
+        Throw "Could not download packages.config."
+    }
+}
+
+# Try find NuGet.exe in path if not exists
+if (!(Test-Path $NUGET_EXE))
+{
+    Write-Verbose -Message "Trying to find nuget.exe in PATH..."
+    $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_ -PathType Container) }
+    $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
+    if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName))
+    {
+        Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
+        $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
+    }
+}
+
+# Try download NuGet.exe if not exists
+if (!(Test-Path $NUGET_EXE))
+{
+    Write-Verbose -Message "Downloading NuGet.exe..."
+    try
+    {
+        $wc = GetProxyEnabledWebClient
+        $wc.DownloadFile($NUGET_URL, $NUGET_EXE)
+    }
+    catch
+    {
+        Throw "Could not download NuGet.exe."
+    }
+}
+
+# Save nuget.exe path to environment to be available to child processed
+$env:NUGET_EXE = $NUGET_EXE
+$env:NUGET_EXE_INVOCATION = if ($IsLinux -or $IsMacOS)
+{
+    "mono `"$NUGET_EXE`""
+}
+else
+{
+    "`"$NUGET_EXE`""
+}
+
+# Restore tools from NuGet?
+if (-Not $SkipToolPackageRestore.IsPresent)
+{
+    Push-Location
+    Set-Location $TOOLS_DIR
+
+    # Check for changes in packages.config and remove installed tools if true.
+    [string] $md5Hash = MD5HashFile $PACKAGES_CONFIG
+    if ((!(Test-Path $PACKAGES_CONFIG_MD5)) -Or
+        ($md5Hash -ne (Get-Content $PACKAGES_CONFIG_MD5 )))
+    {
+        Write-Verbose -Message "Missing or changed package.config hash..."
+        Get-ChildItem -Exclude packages.config, nuget.exe, Cake.Bakery |
+        Remove-Item -Recurse
+    }
+
+    Write-Verbose -Message "Restoring tools from NuGet..."
+
+    $NuGetOutput = Invoke-Expression "& $env:NUGET_EXE_INVOCATION install -ExcludeVersion -OutputDirectory `"$TOOLS_DIR`""
+
+    if ($LASTEXITCODE -ne 0)
+    {
+        Throw "An error occurred while restoring NuGet tools."
     }
     else
     {
-        Write-Host " - Unknown: Cannot continue!"
-        exit 1
+        $md5Hash | Out-File $PACKAGES_CONFIG_MD5 -Encoding "ASCII"
     }
+    Write-Verbose -Message ($NuGetOutput | Out-String)
 
-    if (!$PSScriptRoot)
-    {
-        $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
-    }
-
-    $TOOLS_DIR = Join-Path $PSScriptRoot "tools"
-    $PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
-    $PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
-
-    if ($IsLinuxOS)
-    {
-        # First, we need to fix a bug in Bamboo artifact extraction
-        Write-Host "Fixing artifact extraction - Bamboo extracts Windows paths explictly on Linux"
-        Get-ChildItem -Path $PSScriptRoot |
-        Where-Object {
-            $_.Name.Contains('\')
-        } |
-        Foreach-Object {
-            $linuxPath = $_.Name.Replace('\', '/')
-            $directory = Join-Path $PSScriptRoot ([IO.Path]::GetDirectoryName($linuxPath))
-
-            Write-Host "  $($_.Name) -> $linuxPath"
-
-            if (-not (Test-Path -Path $directory -PathType Container))
-            {
-                New-Item -Path $directory -ItemType Directory | Out-Null
-            }
-
-            [IO.File]::Move($_.FullName, (Join-Path $PSScriptRoot $linuxPath))
-        }
-    }
-
-    # Should we use the new Roslyn?
-    $UseExperimental = "";
-    if ($Experimental.IsPresent)
-    {
-        Write-Verbose -Message "Using experimental version of Roslyn."
-        $UseExperimental = "-experimental"
-    }
-
-    # Is this a dry run?
-    $UseDryRun = "";
-    if ($WhatIf.IsPresent)
-    {
-        $UseDryRun = "-dryrun"
-    }
-
-    # Will we use dotnet to invoke Cake?
-    $DotNet = ""
-    if ($IsLinuxOS)
-    {
-        $DotNet = "dotnet"
-    }
-
-    # Make sure tools folder exists
-    if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR))
-    {
-        Write-Verbose -Message "Creating tools directory..."
-        New-Item -Path $TOOLS_DIR -Type directory | out-null
-    }
-
-    # Make sure that packages.config exist.
-    if (!(Test-Path $PACKAGES_CONFIG))
-    {
-        Write-Verbose -Message "Downloading packages.config..."
-        try
-        {
-            (New-Object System.Net.WebClient).DownloadFile("http://cakebuild.net/download/bootstrapper/packages", $PACKAGES_CONFIG)
-        }
-        catch
-        {
-            Throw "Could not download packages.config."
-        }
-    }
-
-    if ($IsWindowsOS)
-    {
-        # Windows - Acquire Nuget and use to download Cake (.NET Framework)
-
-        $NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
-        $CAKE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
-        $NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-
-        # Try find NuGet.exe in path if not exists
-        if (!(Test-Path $NUGET_EXE))
-        {
-            Write-Verbose -Message "Trying to find nuget.exe in PATH..."
-            $existingPaths = $Env:Path -Split ';' |
-            Where-Object {
-
-                if ([string]::IsNullOrEmpty($_))
-                {
-                    $false
-                }
-
-                try
-                {
-                    # Some paths may throw Access Denied exceptions which write a message to STDERR, causing Bamboo to fail the build
-                    Test-Path $_ -PathType Container
-                }
-                catch
-                {
-                    $false
-                }
-            }
-
-            $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select-Object -First 1
-            if ($null -ne $NUGET_EXE_IN_PATH -and (Test-Path $NUGET_EXE_IN_PATH.FullName))
-            {
-                Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
-                $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
-            }
-        }
-
-        # Try download NuGet.exe if not exists
-        if (!(Test-Path $NUGET_EXE))
-        {
-            Write-Verbose -Message "Downloading NuGet.exe..."
-            try
-            {
-                (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
-            }
-            catch
-            {
-                Throw "Could not download NuGet.exe."
-            }
-        }
-
-        # Save nuget.exe path to environment to be available to child processed
-        $ENV:NUGET_EXE = $NUGET_EXE
-
-        # Restore tools from NuGet?
-        if (-Not $SkipToolPackageRestore.IsPresent)
-        {
-            Push-Location
-            Set-Location $TOOLS_DIR
-
-            # Check for changes in packages.config and remove installed tools if true.
-            [string] $md5Hash = MD5HashFile($PACKAGES_CONFIG)
-            if ((!(Test-Path $PACKAGES_CONFIG_MD5)) -Or
-                ($md5Hash -ne (Get-Content $PACKAGES_CONFIG_MD5 )))
-            {
-                Write-Verbose -Message "Missing or changed package.config hash..."
-                Remove-Item * -Recurse -Exclude packages.config, nuget.exe
-            }
-
-            Write-Verbose -Message "Restoring tools from NuGet..."
-            $NuGetOutput = Invoke-Expression "&`"$NUGET_EXE`" install -ExcludeVersion -OutputDirectory `"$TOOLS_DIR`""
-
-            if ($LASTEXITCODE -ne 0)
-            {
-                Throw "An error occured while restoring NuGet tools."
-            }
-            else
-            {
-                $md5Hash | Out-File $PACKAGES_CONFIG_MD5 -Encoding "ASCII"
-            }
-            Write-Verbose -Message ($NuGetOutput | out-string)
-            Pop-Location
-        }
-
-        # Make sure that Cake has been installed.
-        if (!(Test-Path $CAKE))
-        {
-            Throw "Could not find Cake.exe at $CAKE"
-        }
-    }
-    else
-    {
-        # Linux/CoreCLR
-        # Use dotnet restore to get Cake CoreCLR since we do not have NuGet.exe
-        # Now transform packages.config to a csproj file so that dotnet restore accepts it.
-
-        $xsl = @'
-<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version="1.0"
-    xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    <xsl:template match="/">
-        <Project Sdk="Microsoft.NET.Sdk">
-            <PropertyGroup>
-                <TargetFramework>netstandard2.0</TargetFramework>
-            </PropertyGroup>
-            <ItemGroup>
-                <xsl:for-each select="packages/package">
-                    <PackageReference>
-                        <xsl:attribute name="Include">
-                            <xsl:choose>
-                                <xsl:when test="@id = 'Cake'">Cake.CoreCLR</xsl:when>
-                                <xsl:otherwise><xsl:value-of select="@id" /></xsl:otherwise>
-                            </xsl:choose>
-                        </xsl:attribute>
-                        <xsl:attribute name="Version">
-                            <xsl:value-of select="@version" />
-                        </xsl:attribute>
-                    </PackageReference>
-                </xsl:for-each>
-            </ItemGroup>
-        </Project>
-    </xsl:template>
-</xsl:stylesheet>
-'@
-        $cakeCsproj = Join-Path $PSScriptRoot "cake-package-$([Guid]::NewGuid()).csproj"
-        try
-        {
-            # XSLT transform packages.config to a .csproj file
-            $bytes = [System.Text.Encoding]::UTF8.GetBytes($xsl)
-            $stream = New-Object System.IO.MemoryStream (, $bytes)
-            $reader = [System.Xml.XmlReader]::Create($stream)
-
-            $xsltSettings = New-Object System.Xml.Xsl.XsltSettings;
-            $xmlUrlResolver = New-Object System.Xml.xmlUrlResolver;
-            $xsltSettings.EnableScript = 1;
-
-            $xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
-            $xslt.Load($reader, $xsltSettings, $xmlUrlResolver);
-            $xslt.Transform($PACKAGES_CONFIG, $cakeCsproj);
-
-            # Run dotnet restore
-            & dotnet restore --packages $TOOLS_DIR $cakeCsproj
-
-            # Now locate cake.dll
-            $CAKE = Get-ChildItem -Path $TOOLS_DIR -File -Filter Cake.dll -Recurse |
-            Select-Object -First 1 |
-            Select-Object -ExpandProperty FullName
-
-            if (-not $CAKE)
-            {
-                throw "Could not find Cake.dll (Cake CoreCLR application)"
-            }
-        }
-        catch
-        {
-            $errorMessage = $_.Exception.Message
-            $failedItem = $_.Exception.ItemName
-            Write-Host  'Error'$errorMessage':'$failedItem':' $_.Exception;
-            exit 1
-        }
-        finally
-        {
-            $reader, $stream |
-            Where-Object {
-                $null -ne $_
-            } |
-            Foreach-Object {
-                $_.Dispose()
-            }
-
-            if (Test-Path -Path $cakeCsproj -PathType Leaf)
-            {
-                Remove-Item $cakeCsproj
-            }
-        }
-    }
-
-    # Start Cake
-    Write-Host "Running build script..."
-    Invoke-Expression "& $DotNet `"$CAKE`" `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
-    exit $LASTEXITCODE
+    Pop-Location
 }
-catch
+
+# Restore addins from NuGet
+if (Test-Path $ADDINS_PACKAGES_CONFIG)
 {
-    Write-Host "Exception Thrown: $($_.Exception.Message)"
-    Write-Host $_.ScriptStackTrace
-    exit 1
+    Push-Location
+    Set-Location $ADDINS_DIR
+
+    Write-Verbose -Message "Restoring addins from NuGet..."
+    $NuGetOutput = Invoke-Expression "& $env:NUGET_EXE_INVOCATION install -ExcludeVersion -OutputDirectory `"$ADDINS_DIR`""
+
+    if ($LASTEXITCODE -ne 0)
+    {
+        Throw "An error occurred while restoring NuGet addins."
+    }
+
+    Write-Verbose -Message ($NuGetOutput | Out-String)
+
+    Pop-Location
 }
-finally
+
+# Restore modules from NuGet
+if (Test-Path $MODULES_PACKAGES_CONFIG)
 {
-	Pop-Location
+    Push-Location
+    Set-Location $MODULES_DIR
+
+    Write-Verbose -Message "Restoring modules from NuGet..."
+    $NuGetOutput = Invoke-Expression "& $env:NUGET_EXE_INVOCATION install -ExcludeVersion -OutputDirectory `"$MODULES_DIR`""
+
+    if ($LASTEXITCODE -ne 0)
+    {
+        Throw "An error occurred while restoring NuGet modules."
+    }
+
+    Write-Verbose -Message ($NuGetOutput | Out-String)
+
+    Pop-Location
 }
+
+# Make sure that Cake has been installed.
+if (!(Test-Path $CAKE_EXE))
+{
+    Throw "Could not find Cake.exe at $CAKE_EXE"
+}
+
+$CAKE_EXE_INVOCATION = if ($IsLinux -or $IsMacOS)
+{
+    "mono `"$CAKE_EXE`""
+}
+else
+{
+    "`"$CAKE_EXE`""
+}
+
+
+# Build Cake arguments
+$cakeArguments = @("$Script");
+if ($Target) { $cakeArguments += "-target=$Target" }
+if ($Configuration) { $cakeArguments += "-configuration=$Configuration" }
+if ($Verbosity) { $cakeArguments += "-verbosity=$Verbosity" }
+if ($ShowDescription) { $cakeArguments += "-showdescription" }
+if ($DryRun) { $cakeArguments += "-dryrun" }
+$cakeArguments += $ScriptArgs
+
+# Start Cake
+Write-Host "Running build script..."
+Invoke-Expression "& $CAKE_EXE_INVOCATION $($cakeArguments -join " ")"
+exit $LASTEXITCODE
